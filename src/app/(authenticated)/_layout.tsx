a8bd0af2dd57a23c5cf35250701041ecfebfd7b3
thedatasnok <47036430+thedatasnok@users.noreply.github.com>
2023-10-12 13:32:40 +0200

    feat: wire up active state & navigation for tabs

--- src/app/(authenticated)/_layout.tsx
+++ src/app/(authenticated)/_layout.tsx
@@ -1,7 +1,25 @@
+import Header from '@/components/navigation/Header';
+import NavigationBar from '@/components/navigation/NavigationBar';
+import { View } from '@gluestack-ui/themed';
 import { Slot } from 'expo-router';
+import { ArrowLeftIcon } from 'lucide-react-native';
 
 const AuthenticatedLayout = () => {
-  return <Slot />;
+  return (
+    <>
+      <Header
+        leftIcon={ArrowLeftIcon}
+        context='IDATA2503'
+        title='Course content'
+      />
+
+      <View flex={1}>
+        <Slot />
+      </View>
+
+      <NavigationBar />
+    </>
+  );
 };
 
 export default AuthenticatedLayout;
---
+++ src/app/(authenticated)/courses/index.tsx
@@ -0,0 +1,7 @@
+import { Text } from '@gluestack-ui/themed';
+
+const CoursesScreen = () => {
+  return <Text>Courses</Text>;
+};
+
+export default CoursesScreen;
