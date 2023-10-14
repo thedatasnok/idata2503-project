65119a24c4f7008b7e733a24c0c66892fd575af0
thedatasnok <47036430+thedatasnok@users.noreply.github.com>
2023-10-14 21:33:02 +0200

    feat: add dayjs plugins & norwegian locale

--- src/app/_layout.tsx
+++ src/app/_layout.tsx
@@ -5,6 +5,16 @@ import { Slot } from 'expo-router';
 import { StatusBar } from 'expo-status-bar';
 import { I18nextProvider } from 'react-i18next';
 
+import advancedFormat from 'dayjs/plugin/advancedFormat';
+import relativeTime from 'dayjs/plugin/relativeTime';
+import localizedFormat from 'dayjs/plugin/localizedFormat';
+import dayjs from 'dayjs';
+import 'dayjs/locale/nb';
+
+dayjs.extend(advancedFormat);
+dayjs.extend(relativeTime);
+dayjs.extend(localizedFormat);
+
 const RootLayout = () => {
   return (
     <I18nextProvider i18n={i18n}>
