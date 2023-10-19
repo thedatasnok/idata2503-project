8c5d09c0312ce6f8f26364b777952f8984611845
thedatasnok <47036430+thedatasnok@users.noreply.github.com>
2023-10-19 11:03:39 +0200

    feat: add query client provider to root layout

--- src/app/_layout.tsx
+++ src/app/_layout.tsx
@@ -4,6 +4,7 @@ import { GluestackUIProvider } from '@gluestack-ui/themed';
 import { Slot } from 'expo-router';
 import { StatusBar } from 'expo-status-bar';
 import { I18nextProvider } from 'react-i18next';
+import { QueryClient, QueryClientProvider } from '@tanstack/react-query';
 
 import advancedFormat from 'dayjs/plugin/advancedFormat';
 import relativeTime from 'dayjs/plugin/relativeTime';
@@ -15,14 +16,18 @@ dayjs.extend(advancedFormat);
 dayjs.extend(relativeTime);
 dayjs.extend(localizedFormat);
 
+const queryClient = new QueryClient();
+
 const RootLayout = () => {
   return (
     <I18nextProvider i18n={i18n}>
-      <GluestackUIProvider config={config}>
-        <Slot />
+      <QueryClientProvider client={queryClient}>
+        <GluestackUIProvider config={config}>
+          <Slot />
 
-        <StatusBar />
-      </GluestackUIProvider>
+          <StatusBar />
+        </GluestackUIProvider>
+      </QueryClientProvider>
     </I18nextProvider>
   );
 };
