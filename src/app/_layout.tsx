e6a361e897b18e3d9ec5a81ab625a1f6aae4ff1d
thedatasnok <47036430+thedatasnok@users.noreply.github.com>
2023-10-23 23:29:14 +0200

    fix: #21 utilize splash screen for preventing render prior to determining session state

--- src/app/_layout.tsx
+++ src/app/_layout.tsx
@@ -1,8 +1,9 @@
 import i18n from '@/i18n';
 import config from '@/theme';
-import { GluestackUIProvider } from '@gluestack-ui/themed';
+import { GluestackUIProvider, View } from '@gluestack-ui/themed';
 import { QueryClient, QueryClientProvider } from '@tanstack/react-query';
 import { Slot, router } from 'expo-router';
+import * as SplashScreen from 'expo-splash-screen';
 import { StatusBar } from 'expo-status-bar';
 import { I18nextProvider } from 'react-i18next';
 
@@ -12,39 +13,51 @@ import 'dayjs/locale/nb';
 import advancedFormat from 'dayjs/plugin/advancedFormat';
 import localizedFormat from 'dayjs/plugin/localizedFormat';
 import relativeTime from 'dayjs/plugin/relativeTime';
-import { useEffect } from 'react';
+import { useCallback, useEffect, useState } from 'react';
 
 dayjs.extend(advancedFormat);
 dayjs.extend(relativeTime);
 dayjs.extend(localizedFormat);
 
+SplashScreen.preventAutoHideAsync();
+
 const queryClient = new QueryClient();
 
 const RootLayout = () => {
+  const [ready, setReady] = useState(false);
+
   useEffect(() => {
-    supabase.auth.getSession().then(({ data: { session } }) => {
-      if (session) {
-        router.replace('/(authenticated)');
-      } else {
-        console.log('no user');
+    const prepare = async () => {
+      try {
+        const { data: session } = await supabase.auth.getSession();
+        if (!session) router.replace('/sign-in');
+      } catch (error) {
+        console.warn(error);
+      } finally {
+        setReady(true);
       }
-    });
+    };
 
     supabase.auth.onAuthStateChange((_event, session) => {
-      if (session) {
-        router.replace('/(authenticated)');
-      } else {
-        console.log('no user');
-        router.replace('/(public)/sign-in');
-      }
+      if (!session) router.replace('/sign-in');
     });
+
+    prepare();
   }, []);
+
+  const onAppReady = useCallback(async () => {
+    if (ready) await SplashScreen.hideAsync();
+  }, [ready]);
+
+  if (!ready) return null;
 
   return (
     <I18nextProvider i18n={i18n}>
       <QueryClientProvider client={queryClient}>
         <GluestackUIProvider config={config}>
-          <Slot />
+          <View flex={1} onLayout={onAppReady}>
+            <Slot />
+          </View>
 
           <StatusBar />
         </GluestackUIProvider>
