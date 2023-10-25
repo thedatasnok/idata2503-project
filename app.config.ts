40c5770e279e6a0bfa12145a5b7811dc3442778e
thedatasnok <47036430+thedatasnok@users.noreply.github.com>
2023-10-25 12:45:19 +0200

    build: update bundle identifier for iOS & refactor config to hopefully work with EAS

--- app.config.ts
+++ app.config.ts
@@ -1,47 +1,49 @@
 import type { ExpoConfig } from '@expo/config';
 import 'dotenv/config';
 
-export default {
-  expo: {
-    name: 'whiteboardapp',
-    slug: 'whiteboardapp',
-    scheme: 'whiteboardapp',
-    version: '1.0.0',
-    orientation: 'portrait',
-    icon: './assets/icon.png',
-    userInterfaceStyle: 'light',
-    splash: {
-      image: './assets/splash.png',
-      resizeMode: 'contain',
+const config: ExpoConfig = {
+  name: 'whiteboardapp',
+  slug: 'whiteboardapp',
+  scheme: 'whiteboardapp',
+  version: '1.0.0',
+  orientation: 'portrait',
+  icon: './assets/icon.png',
+  userInterfaceStyle: 'light',
+  splash: {
+    image: './assets/splash.png',
+    resizeMode: 'contain',
+    backgroundColor: '#ffffff',
+  },
+  assetBundlePatterns: ['**/*'],
+  ios: {
+    supportsTablet: true,
+    bundleIdentifier: 'no.overlien.whiteboard',
+  },
+  android: {
+    adaptiveIcon: {
+      foregroundImage: './assets/adaptive-icon.png',
       backgroundColor: '#ffffff',
     },
-    assetBundlePatterns: ['**/*'],
-    ios: {
-      supportsTablet: true,
-      bundleIdentifier: 'no.overlien.whiteboardapp'
+  },
+  experiments: {
+    tsconfigPaths: true,
+    typedRoutes: true,
+  },
+  web: {
+    favicon: './assets/favicon.png',
+  },
+  plugins: ['expo-router'],
+  extra: {
+    supabase: {
+      url: process.env.SUPABASE_URL,
+      anonKey: process.env.SUPABASE_ANON_KEY,
     },
-    android: {
-      adaptiveIcon: {
-        foregroundImage: './assets/adaptive-icon.png',
-        backgroundColor: '#ffffff',
-      },
+    eas: {
+      projectId: 'dd4c825a-9a28-4659-aa35-2ff422814389',
     },
-    experiments: {
-      tsconfigPaths: true,
-      typedRoutes: true,
-    },
-    web: {
-      favicon: './assets/favicon.png',
-    },
-    plugins: ['expo-router'],
-    extra: {
-      supabase: {
-        url: process.env.SUPABASE_URL,
-        anonKey: process.env.SUPABASE_ANON_KEY,
-      },
-      eas: {
-        projectId: 'dd4c825a-9a28-4659-aa35-2ff422814389',
-      },
-    },
-  } satisfies ExpoConfig,
+  },
+};
+
+export default {
+  expo: config,
 };
