f214076a12aa5a5dfdb0c4d34a309aff1f477034
thedatasnok <47036430+thedatasnok@users.noreply.github.com>
2023-10-25 21:46:46 +0200

    fix: fix broken redirection on app load

--- src/app/(authenticated)/_layout.tsx
+++ src/app/(authenticated)/_layout.tsx
@@ -2,12 +2,16 @@ import CoursesDrawerLayout from '@/components/navigation/CoursesDrawerLayout';
 import NavigationBar from '@/components/navigation/NavigationBar';
 import { useAuth } from '@/store/global';
 import { View } from '@gluestack-ui/themed';
-import { Slot } from 'expo-router';
+import { Redirect, Slot, useRootNavigationState } from 'expo-router';
 
 const AuthenticatedLayout = () => {
   const { authenticated } = useAuth();
+  const { key } = useRootNavigationState();
 
-  if (!authenticated) return null;
+  // we gotta wait for the root navigator to be ready before we can
+  // redirect to the sign in page
+  if (key === undefined) return null;
+  if (!authenticated) return <Redirect href='/sign-in' />;
 
   return (
     <>
