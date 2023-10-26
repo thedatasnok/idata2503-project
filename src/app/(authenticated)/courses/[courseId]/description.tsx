596af6c30ce14cec2df8fea03c7222a2fe297183
FennecFromSahara <143624395+FennecFromSahara@users.noreply.github.com>
2023-10-27 00:56:33 +0200

    feat: #26 read data from supabase and styling

--- src/app/(authenticated)/courses/[courseId]/description.tsx
+++ src/app/(authenticated)/courses/[courseId]/description.tsx
@@ -1,35 +1,100 @@
 import Header from '@/components/navigation/Header';
-import { Box, Text } from '@gluestack-ui/themed';
-
-const enum CourseRole {
-  STUDENT = 'STUDENT',
-  ASSISTANT = 'ASSISTANT',
-  LECTURER = 'LECTURER',
-}
-
-// TODO: Replace these with data loaded from backend (from courses props?)
-const course = {
-  id: 'temp',
-  courseCode: 'IDATA2503',
-  name: 'Mobile Applications',
-  description: 'Description',
-};
+import {
+  CourseDescription,
+  CourseMember,
+  useCourseDescription,
+} from '@/services/courses';
+import { Box, Text, Icon } from '@gluestack-ui/themed';
+import { useLocalSearchParams } from 'expo-router';
+import { FlatList, ScrollView, StyleSheet } from 'react-native';
+import { Mail } from 'lucide-react-native';
 
 /**
  * Description of the course, here you can signup if you are not signed up for the course yet
  */
 const CourseDescriptionScreen = () => {
+  const { courseId } = useLocalSearchParams();
+  const { data: courseDescription } = useCourseDescription(courseId as string);
   return (
     <>
-      <Header title='COURSE_CODE' back />
-      <Text>Courses</Text>
-      <Box display='flex' flexDirection='row'>
-        <Text>{course.courseCode}</Text>
-        <Text>{course.name}</Text>
+      <Header
+        context={courseDescription?.course_code}
+        title='Course Description'
+        back
+      />
+      {/* TODO: Inline styles no good? */}
+      <ScrollView style={styles.screenContainer}>
+        <Box display='flex' flexDirection='row'>
+          <Text fontSize={20} fontWeight='bold' paddingTop={10}>
+            {courseDescription?.course_code} {courseDescription?.name}
+          </Text>
+        </Box>
+        <Text fontWeight='$bold'>Course Description</Text>
+        <Text>{courseDescription?.description}</Text>
+
+        <Text fontWeight='$bold'>Lecturers</Text>
+        <FlatList
+          data={courseDescription?.staff}
+          scrollEnabled={false}
+          keyExtractor={(i) => i.user_id}
+          renderItem={({ item: lecturer, index: i }) => (
+            <>
+              <Lecturer
+                key={lecturer.user_id}
+                name={lecturer.name}
+                email={lecturer.email}
+              />
+              {courseDescription?.staff[i + 1] && (
+                <Box h='$px' w='$full' bgColor='$gray200' mt='$2' />
+              )}
+            </>
+          )}
+        />
+      </ScrollView>
+    </>
+  );
+};
+
+interface LecturerProps {
+  name: string;
+  email: string;
+}
+
+const Lecturer: React.FC<LecturerProps> = ({ name, email }) => {
+  return (
+    <>
+      <Box
+        display='flex'
+        flexDirection='row'
+        alignItems='center'
+        px='$1'
+        gap='$1'
+        my='$1'
+      >
+        <Box
+          flex={1}
+          gap={-4}
+          display='flex'
+          flexDirection='row'
+          alignItems='center'
+        >
+          <Box display='flex' flexDirection='column' flexGrow={1}>
+            <Text color='$gray600' fontWeight='$semibold' fontSize='$md'>
+              {name}
+            </Text>
+            <Text fontSize='$xs'>{email}</Text>
+          </Box>
+        </Box>
+        <Icon as={Mail} />
       </Box>
-      <Text>{course.description}</Text>
     </>
   );
 };
 
 export default CourseDescriptionScreen;
+
+const styles = StyleSheet.create({
+  screenContainer: {
+    paddingHorizontal: 12,
+  },
+});
