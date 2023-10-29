37a3cdd15779aa6dc8c97bb2e8dcc0ef223e35f0
thedatasnok <47036430+thedatasnok@users.noreply.github.com>
2023-10-29 23:10:36 +0100

    build: enable onesignal expo plugin

--- app.config.ts
+++ app.config.ts
@@ -32,12 +32,13 @@ const config: ExpoConfig = {
   web: {
     favicon: './assets/favicon.png',
   },
-  plugins: ['expo-router'],
+  plugins: ['expo-router', ['onesignal-expo-plugin', { mode: 'development' }]],
   extra: {
     supabase: {
       url: process.env.SUPABASE_URL,
       anonKey: process.env.SUPABASE_ANON_KEY,
     },
+    oneSignalAppId: process.env.ONESIGNAL_APP_ID,
     eas: process.env.EXPO_EAS_ENABLED && {
       projectId: 'dd4c825a-9a28-4659-aa35-2ff422814389',
     },
