8aa58dd044cc0bc8cb7cb33b2e95f6b5c651d4bd
thedatasnok <47036430+thedatasnok@users.noreply.github.com>
2023-11-01 00:16:00 +0100

    feat(backend): add function for sending push notifications using onesignal

---
+++ supabase/migrations/20231029224941_announcement_notifications.sql
@@ -0,0 +1,43 @@
+CREATE EXTENSION http WITH SCHEMA extensions;
+
+CREATE FUNCTION send_push_notification(headings JSONB, contents JSONB, user_ids UUID[])
+RETURNS INT
+LANGUAGE plpgsql
+AS $$
+DECLARE
+  response_status INT;
+BEGIN
+WITH onesignal AS (
+  SELECT 
+    1 AS grouper,
+    MAX(CASE
+      WHEN ds.name ILIKE '%_token' THEN ds.decrypted_secret
+      ELSE NULL
+    END) AS token,
+    MAX(CASE
+      WHEN ds.name ILIKE '%_app_id' THEN ds.decrypted_secret
+      ELSE NULL
+    END) AS app_id
+  FROM vault.decrypted_secrets ds
+  WHERE 1=1
+    AND (ds.name ILIKE 'onesignal%')
+  GROUP BY grouper
+)
+SELECT INTO response_status status
+FROM onesignal, http((
+    'POST',
+    'https://onesignal.com/api/v1/notifications',
+    ARRAY[http_header('Authorization', 'Bearer ' || onesignal.token)],
+    'application/json',
+    JSON_BUILD_OBJECT(
+      'app_id', onesignal.app_id,
+      'contents', contents,
+      'headings', headers,
+      'target_channel', 'push',
+      'include_aliases', JSONB_BUILD_OBJECT('external_id', user_ids)
+    )
+  )::http_request);
+
+  RETURN response_status;
+END;
+$$;
