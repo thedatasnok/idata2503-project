966074ab5cdce08d4ca74aaaa8e2c179cf84cc1a
thedatasnok <47036430+thedatasnok@users.noreply.github.com>
2023-11-25 00:07:00 +0100

    fix: replace redundant view with fragment & make touchable styleable instead

--- src/components/utils/KeyboardDismissingView.tsx
+++ src/components/utils/KeyboardDismissingView.tsx
@@ -1,10 +1,14 @@
+import { styled } from '@gluestack-ui/themed';
 import { ComponentProps } from 'react';
 import { Keyboard, TouchableWithoutFeedback } from 'react-native';
 import ConfiguredKeyboardAvoidingView from './ConfiguredKeyboardAvoidingView';
-import { View } from '@gluestack-ui/themed';
+
+const StyledKeyboardDismissingView = styled(TouchableWithoutFeedback, {
+  flex: 1,
+});
 
 export interface KeyboardDismissingViewProps
-  extends ComponentProps<typeof View> {}
+  extends ComponentProps<typeof StyledKeyboardDismissingView> {}
 
 /**
  * Component that dismisses the keyboard when pressed within.
@@ -14,9 +18,9 @@ const KeyboardDismissingView: React.FC<KeyboardDismissingViewProps> = ({
   children,
   ...rest
 }) => (
-  <TouchableWithoutFeedback onPress={Keyboard.dismiss}>
-    <View flex={1} {...rest}>{children}</View>
-  </TouchableWithoutFeedback>
+  <StyledKeyboardDismissingView onPress={Keyboard.dismiss} {...rest}>
+    <>{children}</>
+  </StyledKeyboardDismissingView>
 );
 
 export default KeyboardDismissingView;
