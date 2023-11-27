f8ab475ffb4afd35125b7faa970590ea5cfe7563
thedatasnok <47036430+thedatasnok@users.noreply.github.com>
2023-11-27 15:25:46 +0100

    feat: add `isLoading` state to empty state & utilize it in schedule

--- src/components/utils/EmptyState.tsx
+++ src/components/utils/EmptyState.tsx
@@ -1,5 +1,5 @@
 import { IconType } from '@/icon';
-import { Icon, Box, Text } from '@gluestack-ui/themed';
+import { Box, Icon, Spinner, Text } from '@gluestack-ui/themed';
 import { ComponentProps } from 'react';
 
 /**
@@ -7,18 +7,26 @@ import { ComponentProps } from 'react';
  */
 interface EmptyStateProps extends ComponentProps<typeof Box> {
   description: string;
+  isLoading?: boolean;
   icon?: IconType;
 }
 
 const EmptyState: React.FC<EmptyStateProps> = ({
   description,
+  isLoading,
   icon,
   ...rest
 }) => {
   return (
     <Box py='$2' {...rest} alignItems='center' justifyContent='center'>
-      <Icon as={icon} size='3xl' color='$gray700' />
-      <Text color='$gray700'>{description}</Text>
+      {isLoading ? (
+        <Spinner size='large' />
+      ) : (
+        <>
+          <Icon as={icon} size='3xl' color='$gray700' />
+          <Text color='$gray700'>{description}</Text>
+        </>
+      )}
     </Box>
   );
 };
